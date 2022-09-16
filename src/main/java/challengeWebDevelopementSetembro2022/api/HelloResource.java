1878bfb2e31c84d3c341796d3bbcca9fd39e3b95
Eduardo Silva de Freitas Correa <eduardo.correa@first-tech.net>
2022-09-16 17:25:46 -0300

    Question 3 done

--- src/main/java/challengeWebDevelopementSetembro2022/api/HelloResource.java
+++ src/main/java/challengeWebDevelopementSetembro2022/api/HelloResource.java
@@ -1,7 +1,16 @@
 package challengeWebDevelopementSetembro2022.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("/test/hello")
@@ -17,5 +26,35 @@ public class HelloResource
     public ResponseEntity<String> getHelloWithNameParam(@PathVariable("name") String name)
     {
         return (ResponseEntity.ok("Hello World and " + name));
+    }
+
+    @PostMapping(value = "/body", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseBody> postBodyWithName(@Valid @RequestBody ApiRequestBody obj)
+    {
+        return (ResponseEntity.accepted().body(ResponseBody.builder()
+                .message("Hello, " + obj.getName())
+                .processedAt(LocalDateTime.now())
+                .build()));
+    }
+}
+
+@Data
+class ApiRequestBody
+{
+    @NotNull
+    private String name;
+}
+
+@Data
+@Builder
+@AllArgsConstructor
+class ResponseBody
+{
+    private String message;
+    private LocalDateTime processedAt;
+
+    public LocalDateTime getProcessedAt()
+    {
+        return (processedAt.truncatedTo(ChronoUnit.SECONDS));
     }
 }
--- src/test/java/challengeWebDevelopementSetembro2022/api/HelloResourceTest.java
+++ src/test/java/challengeWebDevelopementSetembro2022/api/HelloResourceTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,6 +17,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(HelloResource.class)
 class HelloResourceTest
 {
+    private final static String NAME = "Ada";
+
     @Autowired
     MockMvc mockMvc;
 
@@ -33,13 +37,22 @@ class HelloResourceTest
     @DisplayName("Test getHelloWithNameParam, name = Ada and get String")
     public void testGetHelloWithNameParam() throws Exception
     {
-        String name;
-
-        name = "Ada";
         this.mockMvc.perform(
-                get("/test/hello/" + name))
+                get("/test/hello/" + NAME))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World and " + name));
+                .andExpect(content().string("Hello World and " + NAME));
+    }
+
+    @Test
+    @DisplayName("Test postBodyWithName, name = Ada and get Json Obj")
+    public void testPostBodyWithName() throws Exception
+    {
+       this.mockMvc.perform(
+               post("/test/hello/body").content("{\n\"name\":\""+ NAME +"\"}")
+                       .contentType(MediaType.APPLICATION_JSON))
+               .andDo(print())
+               .andExpect(status().isAccepted())
+               .andExpect(content().string(containsString("{\"message\":\"Hello, Ada\",\"processedAt\":")));
     }
 }
